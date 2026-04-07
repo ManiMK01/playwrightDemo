b3040c2b178c5abe6ccd3a1e5b97f881d0644b59
ManiMK01 <manionce25@gmail.com>
2026-04-07 14:44:25 +0530

    changes

--- customFixture/login.js
+++ customFixture/login.js
@@ -2,11 +2,18 @@ import {test as base} from "@playwright/test"
 
 export const test = base.extend({
     loginFixtuers : async ({page}, use) => {
+
+        /*  It is a Pre-condition setting */
         await page.goto("https://practicetestautomation.com/practice-test-login/")
         await page.getByRole('textbox', {name: 'Username'}).fill('student')
         await page.fill('input#password', 'Password123')
         await page.click('#submit')
         
+        /* use run the test script steps */
         await use()
+
+        /* It is a post-condition setting after script it will execute */
+        await page.click('#logout');  
+        await page.close();   
     }
 })
